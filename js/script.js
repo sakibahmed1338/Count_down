f61089492250c75f7bbb133de2aac13acd36332e
Sakib Ahmed <130605289+sakibahmed1338@users.noreply.github.com>
2023-08-09 16:19:52 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let countDown = new Date("24 December 2023 00:00:00").getTime();
+
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distance = countDown - now ;
+    
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance < 0)
+    {
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+
+
+},1000);
